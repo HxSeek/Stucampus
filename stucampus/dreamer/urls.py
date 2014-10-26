0a094ec8c23f791ea16ce622c7a23dd82d7ac52e
HxSeek <haoxianhehe@gmail.com>
2014-10-26 09:52:29 +0800

    remove the dreamer

--- stucampus/dreamer/urls.py
+++ stucampus/dreamer/urls.py
@@ -5,11 +5,11 @@ from .views import loading, manage_list, app_view
 
 urlpatterns = patterns(
     '',
-    url(r'^$', loading, name='loading'),
-    url(r'^pc/$', PApplyView.as_view(), name='index'),
-    url(r'^pc/success/$', p_success, name='p_success'),
-    url(r'^mobile/$', MApplyView.as_view(), name='mobile_index'),
-    url(r'^mobile/success/$', m_success, name='m_success'),
+    #url(r'^$', loading, name='loading'),
+    #url(r'^pc/$', PApplyView.as_view(), name='index'),
+    #url(r'^pc/success/$', p_success, name='p_success'),
+    #url(r'^mobile/$', MApplyView.as_view(), name='mobile_index'),
+    #url(r'^mobile/success/$', m_success, name='m_success'),
     url(r'^list/$', manage_list, name='list'),
     url(r'^view/$', app_view, name='view'),
 )
